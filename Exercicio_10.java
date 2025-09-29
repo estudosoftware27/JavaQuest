8f3f05391ac3c63eebe664220e46929be1654e5b
unknown <estudosoftware27@gmail.com>
2025-09-29 13:50:28 -0300

    Adicionado mais exercícios

---
+++ Exercicio_10.java
@@ -0,0 +1,19 @@
+package javaquest;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio_10 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
